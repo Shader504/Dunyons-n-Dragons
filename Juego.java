656d4b2f1e670f2f9feaf0da53dc30e9b5282a9f
GGRaaai <raimundomoraga@hotmail.com>
2018-11-02 18:30:34 -0300

    all

--- Juego.java
+++ Juego.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
 public class Juego {
-    
+  private static void menuRaza(){
+    System.out.println("Bienvenido a su Juego de Dunyons & Doragons.");
+    System.out.println("Seleccione raza:");
+    System.out.println("\t1. Humano");
+    System.out.println("\t2. Elfo");
+    System.out.println("\t3. Enano");
+    System.out.println("\t4. Orco");
+  }
+  private static void menuClase(){
+    System.out.println("Seleccione clase:");
+    System.out.println("\t1. Barbaro");
+    System.out.println("\t2. Picaro");
+    System.out.println("\t3. Mago");
+    System.out.println("\t4. Clerigo");
+  }
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    menuRaza();
+    System.out.print("Ingrese numero de raza: ");
+    int raza = scanner.nextInt();
+    Jugador player = new Jugador();
+    //player.asignarRaza(raza);
+    //player.asignarVida(raza);
+    menuClase();
+    System.out.print("Ingrese numero de clase: ");
+    int clase = scanner.nextInt();
+    //player.asignarClase(clase);
+    System.out.print("Ingrese nombre de personaje: ");
+    String nombre = scanner.next();
+    //player.asignarNombre(nombre);
+  }
 }
+
+//
+
+//lets go over there
--- Jugador.java
+++ Jugador.java
@@ -1,14 +1,74 @@
 public class Jugador implements Personaje {
-    void asignarRaza(int opR) {
+    public String name;
+    public String race;
+    public String claz;
+    public int life;
 
+    public void asignarRaza(int opR) {
+      if (opR == 1){
+        Humano humano = new Humano(); //Imagine choosing Human //OMEGALUL
+        this.race = "Humano";
+      }
+      else if (opR == 2){
+        Elfo gnelf = new Elfo();
+        this.race = "Elfo";
+      }
+      else if (opR == 3){
+        Enano enano = new Enano();
+        this.race = "Enano";
+      }
+      else if (opR == 4){
+        Orco orco = new Orco(); //Lok-Tar Ogar
+        this.race = "Orco";
+      }
     }
-    void asignarClase(int opC) {
-
+    public void asignarClase(int opC) {
+        if (opC ==  1) {
+            Barbaro barbaro = new Barbaro();
+            this.claz = "Barbaro";
+        }
+        else if (opC ==  2) {
+            Picaro picaro = new Picaro();
+            this.claz = "Picaro";
+        }
+        else if (opC ==  3) {
+            Mago mago = new Mago();
+            this.claz = "Mago";
+        }
+        else if (opC ==  4) {
+            Clerigo clerigo = new Clerigo();
+            this.claz = "Clerigo";
+        }
     }
-    void asignarVida(int vida) {
-
+    public int asignarVida(int opcion) {
+        if (opcion == 1){
+          this.life = 11;
+        }
+        else if (opcion == 2){
+          this.life = 11;
+        }
+        else if (opcion == 3){
+          this.life = 12;
+        }
+        else if (opcion == 4){
+          this.life = 11;
+        }
     }
-    void asignarNombre(String nombre) {
-
+    public String asignarNombre(String nombre) {
+      this.name = nombre;
     }
 }
+
+
+
+
+/* errores:
+error: Jugador is not abstract and does not override abstract method asignarNombre(String) in Personaje
+
+
+error: Elfo is not abstract and does not override abstract method habilidad() in Raza // en toda raza / clase
+*/
+
+//aun faltan los errores de abstract, eso debe faltar rellenar los otros archivos
+
+//ye
