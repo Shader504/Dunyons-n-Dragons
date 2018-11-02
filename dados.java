8ee83835c8b3ebda9df954e0349300d9839807a7
GGRaaai <raimundomoraga@hotmail.com>
2018-11-02 16:39:44 -0300

    all

---
+++ dados.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+
+
+
+public class dados{
+  public int d20(){
+    Random rand = new Random();
+    int  n = rand.nextInt(20) + 1;
+    return n;
+  }
+  public int d6(){
+    Random rand = new Random();
+    int  n = rand.nextInt(6) + 1;
+    return n;
+  }
+  public int d8(){
+    Random rand = new Random();
+    int  n = rand.nextInt(8) + 1;
+    return n;
+  }
+}
+
+
+
+//ok cool, dps me lo compartes al mail
+//raimundomoraga@hotmail.com
+//brb, gonna go look for food
+
+//
